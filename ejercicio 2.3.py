f44352c814aed4f263fa419c6d366df0a2c480c2
JAMC980 <114441922+JAMC980@users.noreply.github.com>
2023-02-10 19:39:59 +0100

    Add files via upload

---
+++ ejercicio 2.3.py
@@ -0,0 +1,38 @@
+import array
+from typing import ItemsView
+maxSize = 10
+arr = array.array(maxSize)
+
+arr.insert(77)
+arr.insert(99)
+arr.insert("foo")
+arr.insert("bar")
+arr.insert(44)
+arr.insert(55)
+arr.insert(12.34)
+arr.insert(0)
+arr.insert("baz")
+arr.insert(-17)
+
+print("Array containing", len(arr), "items")
+arr.traverse()
+
+print("Search for 12 returns", arr.search(12))
+
+print("Search for 12.34 returns", arr.search(12.34))
+
+print("Deleting 0 returns", arr.delete(0))
+print("Deleting 17 returns", arr.delete(17))
+
+print("Setting item at index 3 to 33")
+arr.set(3, 33)
+
+print("Array after deletions has", len(arr), "items")
+arr.trasverse()
+
+def remove_max_and_sort(lista):
+    lista.remove(max(lista))
+    return sorted(lista, reverse=True)
+
+resultado = remove_max_and_sort(arr)
+print(resultado) 
